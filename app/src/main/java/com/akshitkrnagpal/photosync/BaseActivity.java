d5ba9cc2893a702a70ddde9be3ec08c688a46dd7
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-12-16 20:38:39 +0530

    Created LoginFragment and added to SplashScreenActivity

--- app/src/main/java/com/akshitkrnagpal/photosync/BaseActivity.java
+++ app/src/main/java/com/akshitkrnagpal/photosync/BaseActivity.java
@@ -1,16 +1,16 @@
 package com.akshitkrnagpal.photosync;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-import com.facebook.FacebookSdk;
 
+public class BaseActivity extends FragmentActivity {
 
-public class BaseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
     }
+
+
 }
